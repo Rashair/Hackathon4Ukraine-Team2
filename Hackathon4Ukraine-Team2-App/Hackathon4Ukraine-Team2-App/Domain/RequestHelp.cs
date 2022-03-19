e5593007a880f061713d8debbaf70aaef372672b
Rashair <przemyslaw.wk@gmail.com>
2022-03-19 18:47:57 +0100

    Add sample data for requestHelp

--- Hackathon4Ukraine-Team2-App/Hackathon4Ukraine-Team2-App/Domain/RequestHelp.cs
+++ Hackathon4Ukraine-Team2-App/Hackathon4Ukraine-Team2-App/Domain/RequestHelp.cs
@@ -2,6 +2,8 @@ namespace Hackathon4Ukraine_Team2_App.Domain
 {
     public class RequestHelp
     {
-
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
     }
 }
