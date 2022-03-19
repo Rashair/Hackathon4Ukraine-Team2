56b9f010cf6a5bd76b8fd9c9f8585ccc01482259
Rashair <przemyslaw.wk@gmail.com>
2022-03-19 16:21:47 +0100

    Stworzenie strony Request

---
+++ Hackathon4Ukraine-Team2-App/Hackathon4Ukraine-Team2-App/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hackathon4Ukraine_Team2_App.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
